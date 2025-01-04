521bf5b541033741db319452133ab722b47e6937
Arek Bronowicki <arek@bronowicki.com>
2025-01-04 18:17:47 +0100

    Revert "zmiana jedna"
    This reverts commit f7f7e9aebfb3de23ec81c27e7554c12318cf85b2.

--- raid_on_fort_knox.cpp
+++ raid_on_fort_knox.cpp
@@ -5,7 +5,7 @@
 #include <cmath>    // std::sqrt
 #include <string>
 
-// ----------------- USTAWIENIA ------------------------ zmiana jakaś 
+// ----------------- USTAWIENIA ------------------------
 
 // Rozmiar kafelka (tunel ma 1 kafelek szerokości)
 const int CELL_SIZE = 25;
